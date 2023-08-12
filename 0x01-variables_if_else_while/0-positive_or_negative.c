6bc58ce2e0deac7fcba1a426104f04876631d36e
Abafanamkhize <sfiso04@gmail.com>
2023-08-12 03:11:07 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,27 +3,33 @@
 #include <stdio.h>
 
 /**
- * main -assigning random number numbers.
+ * main - Generating a random number and determining its sign.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int n;
+    int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	}
-	else if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	}
-	else
-	{
-		printf("%d is negative\n", n);
-	}
-	return (0);
+    srand(time(0));
+    n = rand() % (RAND_MAX + 1) - RAND_MAX / 2;
+
+    printf("Last digit of %d is %d ", n, n % 10);
+
+    if (n % 10 > 5)
+    {
+        printf("and is greater than 5");
+    }
+    else if (n % 10 == 0)
+    {
+        printf("and is 0");
+    }
+    else
+    {
+        printf("and is less than 6 and not 0");
+    }
+
+    printf("\n");
+
+    return (0);
 }
