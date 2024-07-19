6dd26361213b250cb5fbbb285b4ab288c44413b3
Abafanamkhize <sfiso04@gmail.com>
2024-07-19 06:38:47 -0700

    3-strcmp.c

--- 0x09-static_libraries/3-strcmp.c
+++ 0x09-static_libraries/3-strcmp.c
@@ -1,24 +1,27 @@
 #include "main.h"
+
 /**
- * _strcmp - compare string values
- * @s1: input value
- * @s2: input value
+ * _strcmp - Compares two strings.
+ * @s1: The first string.
+ * @s2: The second string.
  *
- * Return: s1[i] - s2[i]
+ * Return:
+ *   0 if the strings are equal,
+ *   a negative value if s1 is less than s2,
+ *   a positive value if s1 is greater than s2.
  */
 int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-
-	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0')
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
 		if (s1[i] != s2[i])
-		{
 			return (s1[i] - s2[i]);
-		}
-	i++;
 	}
-	return (0);
+
+	if (s1[i] == '\0' && s2[i] == '\0')
+		return (0);
+
+	return (s1[i] - s2[i]);
 }
